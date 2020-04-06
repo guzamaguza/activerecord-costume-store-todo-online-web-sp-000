c3e397e1d28f8ab1e96127b1fc493e83a829a6ff
Mark A Guza <guzamaguza@gmail.com>
2020-04-06 04:39:01 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -2,8 +2,8 @@
 class CreateCostumes < ActiveRecord::Migration[5.2]
   def change
    create_table :costumes do |t|
-     t.string :name,
-     t.float :price,
+     t.string :name
+     t.float :price
      t.string :size
      t.string :image_url
      t.timestamps null: false
